5b9253d0d24a1201343edee09a1f5c066bdd78e8
Ceres <Ceres445@users.noreply.github.com>
2021-10-01 22:55:08 +0530

    Added file upload script

---
+++ django_simple_bulma/extensions/bulma-file-upload/dist/js/bulma-file-upload.js
@@ -0,0 +1,33 @@
+// Code from https://gist.github.com/micti/bca582bc4054ca7b034faea56930221c
+// Make the bulma file upload work out of the box
+
+document.addEventListener('DOMContentLoaded', () => {
+    // 1. Display file name when select file
+    let fileInputs = document.querySelectorAll('.file.has-name')
+    for (let fileInput of fileInputs) {
+      let input = fileInput.querySelector('.file-input')
+      let name = fileInput.querySelector('.file-name')
+      input.addEventListener('change', () => {
+        let files = input.files
+        if (files.length === 0) {
+          name.innerText = 'No file selected'
+        } else {
+          name.innerText = files[0].name
+        }
+      })
+    }
+  
+    // 2. Remove file name when form reset
+    let forms = document.getElementsByTagName('form')
+    for (let form of forms) {
+      form.addEventListener('reset', () => {
+        console.log('a')
+        let names = form.querySelectorAll('.file-name')
+        for (let name of names) {
+          name.innerText = 'No file selected'
+        }
+      })
+    }
+  })
+
+
